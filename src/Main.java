55a1251a17471b02fa0366fb9e33c1b96e60f94a
MUDJANG <dlsndifn98@gmail.com>
2023-11-22 21:32:53 +0900

    타이틀, 현재 턴 출력 및 숫자 15퍼즐 랜덤 배치 구현
    1. 게임시작 시  타이틀 "재미있는 15퍼즐!" 타이틀 출력
    2. 현재 턴 출력
    3. 숫자 15퍼즐 이중 배열에 랜덤으로 배치

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//1. 타이틀 출력 및 현재턴 표시
+//2. 공백칸을 보함한 1 - 15까지의 숫자를 섞어서 배치(공백을 포함한 숫자 배열을 만들고 이중 배열에 랜덤으로 배치)
+//3. 입력 프롬프트를 구현(입력 유효범위를 1 - 15로 설정, 입력 오류 시 다시 입력받음)
+//4. 정상적으로 입력 시 공백칸 위로 밀어내기(배열 인덱스 값을 확인해 공백칸 배열 인덱스 옆에 위치하는 값이 확인하고 교환)
+//5. 숫자가 올바르게 정렬되면 축하메세지와 함께 게임종료
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("재미있는 15퍼즐!\n");
+
+        int turn = 1;
+        System.out.printf("turn %d",turn);
+        int[][] fifteenPuzzle = new int[4][4];
+
+        for(int i = 0; i < 4 ; i++) {
+            for(int j = 0; j < 4; j++) {
+                fifteenPuzzle[i][j] = (int) (Math.random() * 15);
+            }
+        }
+    }
+}
